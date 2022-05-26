b27747378303167ee40ad682ac746d5c5c87d93c
Domingos Latorre <domingos.latorre@gmail.com>
2022-05-26 11:46:09 -0300

    tests with razor light package

--- Program.cs
+++ Program.cs
@@ -2,12 +2,19 @@
 using LabManager.Database;
 using LabManager.Repositories;
 using LabManager.Models;
+using RazorLight;
+using System.Reflection;
 
 var databaseConfig = new DatabaseConfig();
 
 var databaseSetup = new DatabaseSetup(databaseConfig);
 
 var computerRepository = new ComputerRepository(databaseConfig);
+
+var viewEngine = new RazorLightEngineBuilder()
+    .UseFileSystemProject(Directory.GetCurrentDirectory())
+    .UseMemoryCachingProvider()
+    .Build();
 
 // Routing
 var modelName = args[0];
@@ -17,24 +24,20 @@ if(modelName == "Computer")
 {
     if(modelAction == "List")
     {
-        Console.WriteLine("Computer List");
-        foreach (var computer in computerRepository.GetAll())
-        {
-            Console.WriteLine(
-                "{0}, {1}, {2}", computer.Id, computer.Ram, computer.Processor
-            );
-        }   
+        var computers = computerRepository.GetAll();
+        string result = await viewEngine.CompileRenderAsync("Views/Computer/List.cshtml", computers);
+        Console.WriteLine(result);
     }
 
     if(modelAction == "New")
     {
-        Console.WriteLine("Computer New");
         var id = Convert.ToInt32(args[2]);
         var ram = args[3];
         var processor = args[4];
 
         var computer = new Computer(id, ram, processor);
-        
         computerRepository.Save(computer);
+        string result = await viewEngine.CompileRenderAsync("Views/Computer/Show.cshtml", computer);
+        Console.WriteLine(result);
     }
 }
